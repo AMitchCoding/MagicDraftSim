8c12899bf44054d97aa519aeb42c579a9669ff68
Alex Mitchell <randomguy234@gmail.com>
2015-11-23 21:20:34 -0600

    Fixed Push

---
+++ src/main/java/com/mikeyalex/magicdraftsim/AppController.java
@@ -0,0 +1,17 @@
+package com.mikeyalex.magicdraftsim;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/")
+public class AppController {
+
+    @RequestMapping("/")
+    public String main(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model){
+        model.addAttribute("name", name);
+        return "main";
+    }
+}
